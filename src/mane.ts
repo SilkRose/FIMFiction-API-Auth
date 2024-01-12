f856a4e925bc35f126a115f059d632550b560c2b
Silk Rose <silkrose@love-tolerance.com>
2024-01-12 14:14:17 -0500

    Add parameter test

--- src/mane.ts
+++ src/mane.ts
@@ -1,2 +1,10 @@
 import { Theme, save_theme, set_theme, set_theme_onload } from "./theme";
 import "@total-typescript/ts-reset";
+
+window.onload = mane;
+
+async function mane() {
+	const values = window.location.search;
+	const params = new URLSearchParams(values);
+	params.forEach((p) => console.log(p));
+}
