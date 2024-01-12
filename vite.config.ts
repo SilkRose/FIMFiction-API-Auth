48158775cc7c765c8ed49426e11374bfdbc3506c
Silk Rose <silkrose@love-tolerance.com>
2024-01-12 14:15:01 -0500

    Format with tabs

--- vite.config.ts
+++ vite.config.ts
@@ -4,31 +4,31 @@ import { createHtmlPlugin } from "vite-plugin-html";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  clearScreen: false,
-  root: path.resolve("./src"),
-  publicDir: path.resolve("./public"),
-  build: {
-    outDir: path.resolve("dist"),
-    emptyOutDir: true,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        passes: 3,
-      },
-    },
-  },
-  server: {
-    open: false,
-  },
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-      template: "index.html",
-    }),
-    ViteImageOptimizer({
-      png: {
-        quality: 100,
-      },
-    }),
-  ],
+	clearScreen: false,
+	root: path.resolve("./src"),
+	publicDir: path.resolve("./public"),
+	build: {
+		outDir: path.resolve("dist"),
+		emptyOutDir: true,
+		minify: "terser",
+		terserOptions: {
+			compress: {
+				passes: 3,
+			},
+		},
+	},
+	server: {
+		open: false,
+	},
+	plugins: [
+		createHtmlPlugin({
+			minify: true,
+			template: "index.html",
+		}),
+		ViteImageOptimizer({
+			png: {
+				quality: 100,
+			},
+		}),
+	],
 });
